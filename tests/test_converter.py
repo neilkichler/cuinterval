dcd12f5653a2a5a21e9bbc2ea76a39b92901127f
Neil Kichler <neil.kichler@gmail.com>
2024-02-06 17:01:41 +0100

    further generalization of test converter

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 def convert_to_test(file_path):
     try:
@@ -87,101 +88,105 @@ int main()
 '''
             indent = ' ' * 4
             largest_n = 0
+            supported = ['pos', 'neg', 'add', 'sub', 'mul', 'div', 'sqr', 'sqrt', 'fma']
+            empty = '{empty}'
+            entire = '{entire}'
+            float_max = '0x1.FFFFFFFFFFFFFp1023'
+            float_min = '-0x1.FFFFFFFFFFFFFp1023'
 
             for test in tests:
-                # get the first word as the name of the test
+                # get the first word as the name prefix of the test
                 name, body = test.split(maxsplit=1)
-                name = name.strip("test").strip('_')
-
                 if name.endswith('dec'): # ignore decorated interval tests
                     continue
 
-                body = body.replace('[', '{').replace(']', '}').replace(', ', ',')
+                name = name.strip("test").strip('_')
+                body = body.replace('[', '{').replace(']', '}').replace(', ', ',').replace('=', ' ')
                 ops = body.splitlines()[1:-2]
+                ops = [op.lstrip() for op in sorted(ops)]
 
-                instr = ops[0].split()[0]
-
-                instr_len  = len(ops[0].replace('=', ' ')[:-1].split()) - 1
-                vars = ['ref', 'xs', 'ys', 'zs'][:instr_len]
-                n_vars = len(vars)
-                n_ops = len(ops)
-                var_codes = [''] * n_ops
-
-                test_code = indent + f'"{instr}"_test = [&] {{\n'
-
-                for i in range(n_vars):
-                    ii = (i+1) % n_vars
-                    var_codes[i] = indent + f'    std::array<I, n> h_{vars[ii]} {{{{\n'
-
-                empty = '{empty}'
-                entire = '{entire}'
-                float_max = '0x1.FFFFFFFFFFFFFp1023'
-                float_min = '-0x1.FFFFFFFFFFFFFp1023'
-
+                subtests = defaultdict(list)
                 for op in ops:
-                    if op == '' or '//' in op:
-                        n_ops -= 1
+                    if op in ['', '}'] or '//' in op:
                         continue
-                    
-                    el = op.replace('=', ' ')[:-1].split()
-                    _, *el = el
-
-                    for i, e in enumerate(el):
-                        # check for double max
-                        if e == empty or e == entire:
-                            continue
-
-                        vals = e[1:-1].split(',')
-                        vals = ['std::numeric_limits<T>::max()' if v == float_max else 'std::numeric_limits<T>::lowest()' if v == float_min else v for v in vals]
-                        el[i] = f'{{{vals[0]},{vals[1]}}}'
-
-                    # for i in [(i+1) % n_vars for i in range(n_vars)]:
-                    for i in range(n_vars):
-                        e = el[i]
-                        if e == empty:
-                            var_codes[i] += indent + '        empty,\n'
-                        elif e == entire:
-                            var_codes[i] += indent + '        entire,\n'
-                        else:
-                            var_codes[i] += indent + f'        {e},\n'
-
-                cuda_code = ''
-                for i in range(n_vars):
-                    var_codes[i] += indent + '    }};\n\n'
-
-                for i in range(1, n_vars):
-                    cuda_code += indent + f'    CUDA_CHECK(cudaMemcpy(d_{vars[i]}, h_{vars[i]}.data(), n_bytes, cudaMemcpyHostToDevice));\n'
-
-                device_vars = ''
-                for v in vars[1:]:
-                    device_vars += f', d_{v}'
-
-                cuda_code += indent + f'    test_{instr}<<<numBlocks, blockSize>>>(n{device_vars});\n'
-                cuda_code += indent + f'    CUDA_CHECK(cudaMemcpy(h_{vars[1]}.data(), d_{vars[1]}, n_bytes, cudaMemcpyDeviceToHost));\n'
-                cuda_code += indent + f'    auto failed = check_all_equal<I, n>(h_{vars[1]}, h_ref);\n'
-                cuda_code += indent + '    for (auto fail_id : failed) {\n'
-                cuda_code += indent + '        printf("failed at case %zu:\\n", fail_id);\n'
-                cuda_code += indent + '        printf("'
+                    new_op, body = op.split(maxsplit=1)
+                    subtests[new_op].append(body[:-1].split())
                 
-                params_code = ''
-                for i in range(1, n_vars):
-                    cuda_code += f'{vars[i][0]} = [%a, %a]\\n'
-                    params_code += f', h_{vars[i]}[fail_id].lb, h_{vars[i]}[fail_id].ub'
+                for instr, ops in subtests.items():
+                    instr_len = len(ops[0])
+                    vars = ['ref', 'xs', 'ys', 'zs'][:instr_len]
+                    vars = vars[1:] + vars[:1] # rotate ref to last place
+                    n_vars = len(vars)
+                    n_ops = len(ops)
+                    var_codes = [''] * n_vars
 
-                cuda_code += '"'
-                cuda_code += params_code
-                cuda_code += ');\n'
-                cuda_code += indent + '    }\n'
-                cuda_code += indent + '};\n\n'
+                    if instr not in supported:
+                        print(f'Skipping unsupported instruction: {instr}', file=sys.stderr)
+                        continue
 
-                largest_n = max(n_ops, largest_n)
-                size_code = indent + f'    constexpr int n = {n_ops};\n'
-                test_code += size_code
-                for var_code in var_codes:
-                    test_code += var_code
+                    test_code = indent + f'"{name}_{instr}"_test = [&] {{\n'
 
-                test_code += cuda_code 
-                code += test_code
+                    for i in range(n_vars):
+                        var_codes[i] = indent + f'    std::array<I, n> h_{vars[i]} {{{{\n'
+
+                    for op in ops:
+                        intervals = op
+
+                        for i, interval in enumerate(intervals):
+                            # check for double max
+                            if interval == empty or interval == entire:
+                                continue
+
+                            vals = interval[1:-1].split(',')
+                            vals = ['std::numeric_limits<T>::max()' if v == float_max else 'std::numeric_limits<T>::lowest()' if v == float_min else v for v in vals]
+                            intervals[i] = f'{{{vals[0]},{vals[1]}}}'
+
+                        for i in range(n_vars):
+                            interval = intervals[i]
+                            if interval == empty:
+                                var_codes[i] += indent*3 + 'empty,\n'
+                            elif interval == entire:
+                                var_codes[i] += indent*3 + 'entire,\n'
+                            else:
+                                var_codes[i] += indent*3 + f'{interval},\n'
+
+                    cuda_code = ''
+                    for i in range(n_vars):
+                        var_codes[i] += indent*2 + '}};\n\n'
+
+                    for i in range(n_vars - 1):
+                        cuda_code += indent*2 + f'CUDA_CHECK(cudaMemcpy(d_{vars[i]}, h_{vars[i]}.data(), n_bytes, cudaMemcpyHostToDevice));\n'
+
+                    device_vars = ''
+                    for v in vars[:-1]:
+                        device_vars += f', d_{v}'
+
+                    cuda_code += indent*2 + f'test_{instr}<<<numBlocks, blockSize>>>(n{device_vars});\n'
+                    cuda_code += indent*2 + f'CUDA_CHECK(cudaMemcpy(h_{vars[0]}.data(), d_{vars[0]}, n_bytes, cudaMemcpyDeviceToHost));\n'
+                    cuda_code += indent*2 + f'auto failed = check_all_equal<I, n>(h_{vars[0]}, h_ref);\n'
+                    cuda_code += indent*2 + 'for (auto fail_id : failed) {\n'
+                    cuda_code += indent*3 + 'printf("failed at case %zu:\\n", fail_id);\n'
+                    cuda_code += indent*3 + 'printf("'
+                    
+                    params_code = ''
+                    for i in range(1, n_vars):
+                        cuda_code += f'{vars[i][0]} = [%a, %a]\\n'
+                        params_code += f', h_{vars[i]}[fail_id].lb, h_{vars[i]}[fail_id].ub'
+
+                    cuda_code += '"'
+                    cuda_code += params_code
+                    cuda_code += ');\n'
+                    cuda_code += indent*2 + '}\n'
+                    cuda_code += indent + '};\n\n'
+
+                    largest_n = max(n_ops, largest_n)
+                    size_code = indent*2 + f'constexpr int n = {n_ops};\n'
+                    test_code += size_code
+                    for var_code in var_codes:
+                        test_code += var_code
+
+                    test_code += cuda_code 
+                    code += test_code
 
             code_constants = f'''
     const int n = {largest_n}; // count of largest test array
@@ -202,15 +207,25 @@ int main()
 
 if __name__ == '__main__':
 
-    # get file path from command line
+    # get input file path from command line
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
     else:
         file_path = 'libieeep1788_elem.itl'
 
+    # get output file path from command line (default: stdout)
+    if len(sys.argv) > 2:
+        out_file = sys.argv[2]
+    else:
+        out_file = None
+
     test_code = convert_to_test(file_path)
 
-    # write to file
-    with open('tests.cu', 'w') as f:
+    if out_file:
+        with open(out_file, 'w') as f:
+            f.write(test_code)
+    else:
+        f = sys.stdout
         f.write(test_code)
+
 
