e249d4cc1a87d5b5cff54e4776f5668abf236192
Neil Kichler <neil.kichler@gmail.com>
2024-03-07 19:45:45 +0100

    add absorption of connected root interval solutions

--- examples/bisection.cu
+++ examples/bisection.cu
@@ -9,16 +9,21 @@
 template<typename I>
 __device__ I f(I x)
 {
-    return exp(I { -3.0, -3.0 } * x) - sin(x) * sin(x) * sin(x);
+    // return exp(I { -3.0, -3.0 } * x) - sin(x) * sin(x) * sin(x);
+    // return I{1.0, 1.0};
+    // return x*sqr(x) - (I{2.0, 2.0} * sqr(x)) + x;
+    // return sqr(sin(x)) - (I{1.0, 1.0} - cos(I{2.0, 2.0} * x)) / I{2.0, 2.0};
+    return pown(x, 3) - pown(x, 2) - I{17.0, 17.0} * x - I{15.0, 15.0};
 };
 
 void example_bisection()
 {
     using I = interval<double>;
 
-    I x = { -10.0, 10.0 };
+    // I x = { -1.0, 2.0 };
+    I x = { -5.0, 10.0 };
 
-    constexpr double tolerance      = 1e-7;
+    constexpr double tolerance      = 1e-12;
     constexpr std::size_t max_depth = 512;
     std::size_t max_roots           = 16;
 
@@ -30,7 +35,7 @@ void example_bisection()
     CUDA_CHECK(cudaMalloc(&d_roots, max_roots * sizeof(*d_roots)));
     bisection<double, max_depth><<<1, 1>>>(x, tolerance, d_roots, d_max_roots);
     CUDA_CHECK(cudaMemcpy(&max_roots, d_max_roots, sizeof(*d_max_roots), cudaMemcpyDeviceToHost));
-    printf("We found %zu roots:\n", max_roots);
+    printf("We found %zu roots.\n", max_roots);
     
     I *h_roots = (I *)std::malloc(max_roots * sizeof(*h_roots));
     CUDA_CHECK(cudaMemcpy(h_roots, d_roots, max_roots * sizeof(I), cudaMemcpyDeviceToHost));
--- examples/cuinterval/examples/bisection.cuh
+++ examples/cuinterval/examples/bisection.cuh
@@ -18,6 +18,8 @@ struct local_stack
     size_type len {};
 };
 
+#include <cstdio>
+
 // Example implementation of the bisection method for finding all roots in a given interval.
 template<typename T, int max_depth>
 __global__ void bisection(interval<T> x_init, double tol, interval<T> *roots, std::size_t *max_roots)
@@ -32,25 +34,54 @@ __global__ void bisection(interval<T> x_init, double tol, interval<T> *roots, st
         I x = intervals.pop();
         I y = f(x);
 
+        // printf("x = [%f, %f], y = [%f, %f]\n", x.lb, x.ub, y.lb, y.ub);
+
         if (!contains(y, 0.0)) {
+            // printf("no roots in [%f, %f]\n", x.lb, x.ub);
             continue; // no roots in this interval -> no further splitting
         }
 
         T m = mid(x);
         if (width(x) < tol || m == inf(x) || m == sup(x)) {
             // found a root
-            roots[n_roots] = x;
-            n_roots++;
+
+            // try to absorb the root if close to an existing root interval
+            bool absorbed = false;
+            for (std::size_t i = 0; i < n_roots; i++) {
+                I root = roots[i];
+                if (inf(root) <= sup(x) && sup(x) <= sup(root)
+                ||  inf(root) <= inf(x) && inf(x) <= sup(root)
+                ||  inf(x) <= sup(root) && sup(root) <= sup(x)
+                ||  inf(x) <= inf(root) && inf(root) <= sup(x)) {
+                    roots[i] = convex_hull(root, x);
+                    absorbed = true;
+                    // printf("absorbed root at = %f, %f with new root = %f, %f\n", root.lb, root.ub, x.lb, x.ub);
+                    break;
+                }
+            }
+
+            if (!absorbed) {
+                roots[n_roots] = x;
+                // printf("found root at = %f, %f\n", x.lb, x.ub);
+                n_roots++;
+            }
+
+
             if (n_roots == *max_roots) {
+                // printf("Reached max_roots = %zu\n", *max_roots);
                 break; // reached max roots we can store
             }
         } else {
             // interval could still contain a root -> bisect
             split<T> c = bisect(x, 0.5);
+            // we do depth-first search which often will not be optimal
             intervals.push(c.upper_half);
             intervals.push(c.lower_half);
+            // printf("bisect = [%f, %f] -> ([%f, %f], [%f, %f])\n", x.lb, x.ub, c.lower_half.lb, c.lower_half.ub, c.upper_half.lb, c.upper_half.ub);
         }
     }
+
+    // printf("n_roots = %d\n", (int)n_roots);
 
     *max_roots = n_roots;
 }
