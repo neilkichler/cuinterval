0b769a023ac60923ee5846d894db5f3c5581ade2
Neil Kichler <neil.kichler@gmail.com>
2024-03-12 15:13:54 +0100

    cleanup

--- examples/bisection.cu
+++ examples/bisection.cu
@@ -24,18 +24,12 @@ void example_bisection()
     CUDA_CHECK(cudaMalloc(&d_roots, max_roots * sizeof(*d_roots)));
     bisection<double, max_depth><<<1, 1>>>(x, tolerance, d_roots, d_max_roots);
     CUDA_CHECK(cudaMemcpy(&max_roots, d_max_roots, sizeof(*d_max_roots), cudaMemcpyDeviceToHost));
-    printf("We found %zu roots.\n", max_roots);
 
     I *h_roots = (I *)std::malloc(max_roots * sizeof(*h_roots));
     CUDA_CHECK(cudaMemcpy(h_roots, d_roots, max_roots * sizeof(I), cudaMemcpyDeviceToHost));
     for (std::size_t i = 0; i < max_roots; i++) {
         printf("Root %zu in [%.15f, %.15f]\n", i, h_roots[i].lb, h_roots[i].ub);
-
-
-        printf("diff is: %.15f\n", h_roots[i].ub - h_roots[i].lb);
-        printf("tolerance is: %.15f\n", tolerance);
     }
-
 
     std::free(h_roots);
     CUDA_CHECK(cudaFree(d_roots));
