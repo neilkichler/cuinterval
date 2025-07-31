222f207d7961fa017223852b89d669906e6f803d
Neil Kichler <neil.kichler@gmail.com>
2025-07-31 18:43:11 +0200

    add numeric_limits specialization for intervals

---
+++ examples/limits/limits.cu
@@ -0,0 +1,17 @@
+#include <cuinterval/cuinterval.h>
+
+#include <cuinterval/limits.h>
+
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    using interval_limits = std::numeric_limits<cu::interval<double>>;
+
+    std::cout << std::format("interval<double> min: {}\n", interval_limits::min());
+    std::cout << std::format("interval<double> max: {}\n", interval_limits::max());
+    std::cout << std::format("interval<double> eps: {}\n", interval_limits::epsilon());
+
+    return 0;
+}
