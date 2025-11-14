9001e0ff99234f50a31232cb051fc1b1a2a80c43
Neil Kichler <neil.kichler@gmail.com>
2025-11-14 15:08:09 +0100

    docs: gen.sh fix shellcheck warnings

--- docs/gen/gen.sh
+++ docs/gen/gen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-base_path=$(CDPATH= cd -- "$(dirname -- "$0")/../" && pwd -P)
+base_path=$(CDPATH='' cd -- "$(dirname -- "$0")/../" && pwd -P)
 doc_path="$base_path/src/content/docs"
 gen_path="$base_path/gen"
-cd $gen_path
+cd "$gen_path" || exit
 python3 gen_functions.py
 python3 gen_docs.py
 mv "api.mdx" "$doc_path/reference"
-mv *.mdx "$doc_path/operations"
+mv -- *.mdx "$doc_path/operations"
