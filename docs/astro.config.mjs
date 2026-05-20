979ac8c1dcaac5f03fa3981bc9dbc96300304076
Neil Kichler <neil.kichler@gmail.com>
2026-05-20 13:10:40 +0200

    docs: update astro to 6.x

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,74 +1,78 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 import path from "node:path";
-import starlight from '@astrojs/starlight';
-import starlightLinksValidator from 'starlight-links-validator';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import starlightThemeFlexoki from 'starlight-theme-flexoki'
+import starlight from "@astrojs/starlight";
+import starlightLinksValidator from "starlight-links-validator";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import starlightThemeFlexoki from "starlight-theme-flexoki";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://neilkichler.github.io',
-	base: '/cuinterval',
-	prefetch: {
-		prefetchAll: true
-	},
-	env: {
-		schema: {
-			PUBLIC_VERSION: envField.string({
-				context: 'client',
-				access: 'public',
-				optional: false
-			})
-		},
-		validateSecrets: true
-	},
-	vite: {
-		resolve: {
-			alias: {
-				"@assets": path.resolve("./src/assets"),
-				"@components": path.resolve("./src/components"),
-			}
-		},
-	},
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
-	},
-	integrations: [
-		starlight({
-			favicon: '/favicon.png',
-			title: 'CuInterval',
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/neilkichler/cuinterval' },
-			],
-			customCss: ['katex/dist/katex.min.css'],
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Operations',
-					autogenerate: { directory: 'operations' },
-					collapsed: true
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Resources',
-					autogenerate: { directory: 'resources' },
-				},
-			],
-			plugins: [
-				starlightLinksValidator({
-					errorOnRelativeLinks: false,
-				}),
-				starlightThemeFlexoki()
-			]
-		}),
-	],
+    site: "https://neilkichler.github.io",
+    base: "/cuinterval",
+    prefetch: {
+        prefetchAll: true,
+    },
+    env: {
+        schema: {
+            PUBLIC_VERSION: envField.string({
+                context: "client",
+                access: "public",
+                optional: false,
+            }),
+        },
+        validateSecrets: true,
+    },
+    vite: {
+        resolve: {
+            alias: {
+                "@assets": path.resolve("./src/assets"),
+                "@components": path.resolve("./src/components"),
+            },
+        },
+    },
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    },
+    integrations: [
+        starlight({
+            favicon: "/favicon.png",
+            title: "CuInterval",
+            social: [
+                {
+                    icon: "github",
+                    label: "GitHub",
+                    href: "https://github.com/neilkichler/cuinterval",
+                },
+            ],
+            customCss: ["katex/dist/katex.min.css"],
+            sidebar: [
+                {
+                    label: "Guides",
+                    items: [{ autogenerate: { directory: "guides" } }],
+                },
+                {
+                    label: "Operations",
+                    items: [{ autogenerate: { directory: "operations" } }],
+                    collapsed: true,
+                },
+                {
+                    label: "Reference",
+                    items: [{ autogenerate: { directory: "reference" } }],
+                },
+                {
+                    label: "Resources",
+                    items: [{ autogenerate: { directory: "resources" } }],
+                },
+            ],
+            plugins: [
+                starlightLinksValidator({
+                    errorOnRelativeLinks: false,
+                }),
+                starlightThemeFlexoki(),
+            ],
+        }),
+    ],
 });
