c7ac35357ff529cbaabfda532d745b15b02bc9a5
Neil Kichler <neil.kichler@gmail.com>
2026-07-17 17:29:25 +0200

    operations: move isnai and isnan upwards

--- include/cuinterval/arithmetic/operations.cuh
+++ include/cuinterval/arithmetic/operations.cuh
@@ -14,6 +14,20 @@
 namespace cu
 {
 
+// is not an interval
+template<typename T>
+inline constexpr __device__ __host__ bool isnai(interval<T> x)
+{
+    return x.lb != x.lb && x.ub != x.ub;
+}
+
+// is not a number is equivalent to isnai
+template<typename T>
+inline constexpr __device__ __host__ bool isnan(interval<T> x)
+{
+    return isnai(x);
+}
+
 //
 // Constant intervals
 //
@@ -696,20 +710,6 @@ inline constexpr __device__ __host__ bool isinf(interval<T> x)
 {
     using intrinsic::neg_inf, intrinsic::pos_inf;
     return x.lb == neg_inf<T>() || x.ub == pos_inf<T>();
-}
-
-// is not an interval
-template<typename T>
-inline constexpr __device__ __host__ bool isnai(interval<T> x)
-{
-    return x.lb != x.lb && x.ub != x.ub;
-}
-
-// is not a number is equivalent to isnai
-template<typename T>
-inline constexpr __device__ __host__ bool isnan(interval<T> x)
-{
-    return isnai(x);
 }
 
 template<typename T>
