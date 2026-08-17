e9b6a3220a16e7a03f09531e24e281a3b04f7941
Neil Kichler <neil.kichler@gmail.com>
2026-08-17 17:02:18 +0200

    cuinterval.h: only include .cuh files when compiling CUDA code

--- include/cuinterval/cuinterval.h
+++ include/cuinterval/cuinterval.h
@@ -1,8 +1,11 @@
 #ifndef CUINTERVAL_H
 #define CUINTERVAL_H
 
+#ifdef __CUDACC__
 #include <cuinterval/arithmetic/intrinsics.cuh>
 #include <cuinterval/arithmetic/operations.cuh>
+#endif
+
 #include <cuinterval/compare.h>
 #include <cuinterval/interval.h>
 #include <cuinterval/numbers.h>
